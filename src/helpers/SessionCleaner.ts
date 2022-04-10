c9fe27ed90754ed1693451308c0eed49eaee5738
luka <leandr1999@gmail.com>
2022-04-10 15:05:01 +0200

    feat: On each start, server will remove all previous keys, certs and other data

--- src/helpers/SessionCleaner.ts
+++ src/helpers/SessionCleaner.ts
@@ -3,24 +3,16 @@ import * as fs from "fs";
 
 export function sessionCleaner() {
     const appTempDir = os.tmpdir() + '/.secret-agent';
-    const specialFiles = [
-        'ca.der',
-        'caKey.der',
-        'network.db',
-        'privKey.der',
-        'sessions.db',
-    ];
 
     fs.readdir(appTempDir, (err, files) => {
 
         files.forEach(fileName => {
             const pathToFile = appTempDir + '/' + fileName;
-            if (!specialFiles.includes(fileName)) {
-                fs.unlink(pathToFile, () => {});
-            }
+            fs.unlink(pathToFile, () => {
+            });
         });
     });
 
-    console.log('Session Files Cleaned');
+    console.log('Previous Session Files Cleaned');
 }
 
