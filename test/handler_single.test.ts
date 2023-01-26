9ea41441a2b2f9446513d04d430deefa0253a8d7
LuKa <leandr1999@gmail.com>
2023-01-26 09:05:51 -0500

    test: creepjs test

--- test/handler_single.test.ts
+++ test/handler_single.test.ts
@@ -35,4 +35,39 @@ describe('Hero single thread', () => {
                 done();
             });
     });
+
+    test('creepjs test', (done) => {
+        const script = async (agent: Hero, resolve: (output?: any) => void, reject: (error?: any) => void) => {
+            await agent.goto('https://abrahamjuliot.github.io/creepjs/');
+            await agent.waitForPaintingStable();
+            await agent.waitForMillis(3000);
+            await agent.waitForElement(agent.document.querySelector('div.visitor-info'))
+            resolve(
+                parseFloat((
+                        await agent.document
+                            .querySelector('#fingerprint-data > div.visitor-info > div > div:nth-child(2) > div:nth-child(2) > span')
+                            .textContent
+                    )
+                        ?.replace('%', '').trim()
+                    ??
+                    '0'
+                )
+            )
+        };
+
+        handler.process(
+            new Task(script
+                .toString()
+                .substring(
+                    script.toString().indexOf('{') + 1,
+                    script.toString().length - 1
+                )
+                .trim()
+            ),
+            (task: Task) => {
+                expect(task.status).toBe(TaskStatus.DONE);
+                expect(task.output).toBeGreaterThan(50);
+                done();
+            });
+    }, 10000);
 });
