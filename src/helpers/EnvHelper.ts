284478dbad76fb5d89d8a7fdc2c772780d1ac64d
LuKa <leandr1999@gmail.com>
2023-03-12 19:33:55 -0400

    feat: envString helper

--- src/helpers/EnvHelper.ts
+++ src/helpers/EnvHelper.ts
@@ -6,3 +6,8 @@ export function envInt(key: string): number | null {
 export function envBool(key: string): boolean {
     return process.env[key] === 'true' || process.env[key] === '1';
 }
+
+export function envString(key: string): string | null {
+    const env = process.env[key];
+    return (env !== undefined && env.length) ? env : null;
+}
