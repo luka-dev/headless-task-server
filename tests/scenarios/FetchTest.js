fd807b8bb5d4c2f144fae7224c3b344aa24c128a
LuKa <leandr1999@gmail.com>
2025-07-06 04:42:07 -0400

    upd: update FetchTest to validate timeout behavior with new endpoint

--- tests/scenarios/FetchTest.js
+++ tests/scenarios/FetchTest.js
@@ -1,6 +1,13 @@
-await agent.goto('https://developer.mozilla.org/');
+await agent.goto('https://httpbin.org/');
 await agent.waitForPaintingStable();
-await agent.waitForMillis(10000);
-const response = await agent.fetch('https://developer.mozilla.org/api/v1/whoami', {timeout: 10000});
-await agent.waitForMillis(10000);
-resolve(await response.json());
+
+try {
+    await agent.fetch('https://httpbin.org/delay/30', {timeout: 10000});
+    reject('Fetch did not timeout as expected');
+}
+catch (e) {
+    resolve('Fetch timed out as expected');
+    return;
+}
+
+reject('Fetch did not time out as expected');
